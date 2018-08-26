c2bedd1462329d57eaa4ea8536325afe87f0ac49
nenadmitrovic <nenadmitrovic04@gmail.com>
2018-08-26 23:40:52 +0200

    add incf function
    This function increases the count of feature/category pair

--- src/clj/spam_filter/routes/home.clj
+++ src/clj/spam_filter/routes/home.clj
@@ -11,6 +11,7 @@
 
 
 
+(def doc "The reason Bayesian classifiers are often used for document classification is that they require far less computing power than other methods do. An email message might have hundreds or even thousands of words in it, and simply updating the counts takes vastly less memory and processor cycles than training a neural network of that size does; as shown, it can be done entirely within a database. Depending on the speed required for training and querying, and on the environment in which it is run, a neural network may be a viable alternative. ")
 
 
 
@@ -24,20 +25,21 @@
     (reduce (fn [final-map word]
               (assoc final-map word 1))
             {}
-            final-words)))
+            final-words))) 
 
 
 
-
-
-
-
-
-
-
-
-
-
+(defn incf
+  "This function increases the count of feature/category pair"
+  [f cat fc]
+  (let [map1 (if (= (get fc f) nil)
+               (assoc fc f {})
+               fc)]
+(if (get-in map1 [f cat])
+  (assoc-in map1 [f cat] (inc (get-in map1 [f cat])))
+  (do
+    (let [new-val (assoc-in map1 [f cat] 0)]
+      (assoc-in new-val [f cat] (inc (get-in new-val [f cat]))))))))
 
 
 
