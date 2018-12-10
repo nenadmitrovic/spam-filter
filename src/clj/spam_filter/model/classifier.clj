333ed97cf1384d9cf65e5ad7bad50ba14ee39acf
nenadmitrovic <nenadmitrovic04@gmail.com>
2018-12-10 01:09:40 +0100

    Add docprob, prob, settreshold, gettgreshold functions

--- src/clj/spam_filter/model/classifier.clj
+++ src/clj/spam_filter/model/classifier.clj
@@ -1,16 +1,14 @@
 (ns spam-filter.model.classifier
   (:require [clojure.string :as st]))
 
-
+; Counts of feature/category combinations
 (def fc (atom {}))
 
 
-@fc
-
+; Counts of documents in each category
+; How many times every classification has been used
 (def cc (atom {}))
 
-
-@cc
 
 ; extracts features from the text
 (defn getwords
@@ -21,21 +19,20 @@
         less-than-20 (filter #(< (count %) 20) words)
         final-words (filter #(> (count %) 2) less-than-20)]
     (reduce (fn [final-map word]
-              (assoc final-map word 1))
+              (assoc final-map (.toLowerCase word) 1))
             {}
-            final-words))) 
+            final-words)))
 
 
 ; increase the count of a feature/category pair
 (defn incf
-  [f cat]
+  [f cat] 
 (if (not (contains? @fc f))
   (swap! fc #(assoc % f {})))
 (if (not (get-in @fc [f cat]))
   (swap! fc #(assoc-in % [f cat] 0)))
 (swap! fc #(update-in % [f cat] inc)))
 
-(incf "jumps" "")
 
 
 ; increase the count of a category
@@ -44,7 +41,6 @@
 (if (not (contains? @cc cat))
 (swap! cc #(assoc % cat 0)))
 (swap! cc #(update % cat inc)))
-
 
 
 ; The number of times a feature has appeared in a category
@@ -65,7 +61,7 @@
     n-of-items
     0)))
 
-(catcount "bad")
+
 
 
 ; The total numbers of items
@@ -75,12 +71,14 @@
 
 
 
+
 ; The list of all categories
 (defn categories
 []
 (keys @cc))
 
 
+@cc
 
 (defn train
   [t cat]
@@ -88,8 +86,13 @@
 (let [ws (keys (getwords t))]
   (for [w ws] (incf w cat))))
 
-(train "jumps fox" "good")
 
+
+(defn train1
+  [t cat]
+  (incc cat)
+  (let [features (keys (getwords t))]
+    (map incf features (repeat (count features) cat))))
 
 
 
@@ -97,12 +100,10 @@
 []
 (do
   [(train "Nobody owns the water." "good")
-  (train "the quick rabit jumps fences" "good")
+  (train "the quick rabbit jumps fences" "good")
   (train "buy pharmaceuticals now" "bad")
   (train "make quick money at the online casino" "bad")
   (train "the quick brown fox jumps" "good")]))
-
-(sampletrain)
 
 
 
@@ -115,9 +116,8 @@
 
 
 
-
 (defn weightedprob
-  [f cat]
+  [f cat fprob]
 (let [weight 1
       ap 0.5
       basicprob (fprob f cat)
@@ -127,6 +127,44 @@ bp))
 
 
 
+; Extracts features and multiplies all
+; their probabilities together to get
+; an overall probability Pr(Document | Category)
+
+(defn docprob
+  [item cat]
+  (let [features (keys (getwords item))]
+
+  (loop [features features
+         p 1]
+    (if (empty? features)
+      p
+      (recur
+       (rest features)
+       (* p (weightedprob (first features) cat fprob)))))))
+
+
+
+(defn prob
+  [item cat]
+  (let [catprob (/ (catcount cat) (totalcount))
+        docprob (docprob item cat)]
+    (* docprob catprob)))
+
+
+(def thresholds (atom {}))
+
+
+(defn setthreshold
+  [cat t]
+(swap! thresholds #(assoc % cat t)))
+
+
+(defn getthreshold
+  [cat]
+(if (contains? @thresholds cat)
+  (get @thresholds cat)
+1.0))
 
 
 
@@ -158,7 +196,29 @@ bp))
 
 
 
-          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
